ea9fd898e952d77cf97ff587b3d941616c8dd269
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-05-13 16:05:08 +0000

    vuelve jere

--- opcodes.c
+++ opcodes.c
@@ -44,3 +44,14 @@ void pop_f(stack_t **stack, unsigned int l_count)
   free(ext.stack);
   ext.stack = aux;
 }
+
+/**
+ * nop_f - entry point
+ * @stack: the opcode
+ * @l_count: function to handle the opcode
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+void nop_f(stack_t **stack __attribute__((unused)), unsigned int l_count __attribute__((unused)))
+{
+}
